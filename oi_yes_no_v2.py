ab18908684ba0b57c8e28c31c272913a31b8761a
Estrangedd <nai.tevit@awatapu.school.nz>
2021-07-26 12:52:05 +1200

    YES NO CHECK

--- oi_yes_no_v2.py
+++ oi_yes_no_v2.py
@@ -5,18 +5,13 @@ while show_instructions.lower() !="xxx":
     show_instructions =  input("have you played this game"
                               "before? ").lower()
     # If they say yes, output 'program continues'
-    if show_instructions == "yes":
+    if show_instructions == "yes" or show_instructions == "y"
+        Show_instructions = "yes"
         print("program continues")
 
-    elif show_instructions == "y":
-        print("display instructions")
 
-    elif show_instructions == "no":
+    elif show_instructions == "no" or show_instructions == "n":
         print("display instructions")
-
-    elif show_instructions == "n":
-        print("display instructions")
-
 
 
     # If they say no,output 'display instructions'
