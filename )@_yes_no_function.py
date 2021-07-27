c67d4a033ca3056aabad6256376dd919c89e7de7
Estrangedd <nai.tevit@awatapu.school.nz>
2021-07-27 15:03:45 +1200

    YES NO FUNCTION

---
+++ )@_yes_no_function.py
@@ -0,0 +1,26 @@
+# Function go here...
+def yes_no(question):
+    valid = False
+    while not valid:
+        response =  input(question).lower()
+
+        if response == "yes" or response  == "y":
+            response="yes"
+            return response
+
+        elif response  == "no" or response  == "n":
+            response = "no"
+            return response
+        else:
+            print("Please answer yes / no")
+
+
+
+
+# Main Routine goes here
+show_instrictions = yes_no("Have you played the "
+                           "game before")
+print("You chose {}" .format(show_instrictions))
+print()
+having_fun = yes_no("Are you having fun?")
+print("you said {} to having fun".format(having_fun))
